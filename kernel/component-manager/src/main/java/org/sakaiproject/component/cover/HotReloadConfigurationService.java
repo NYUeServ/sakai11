1c8b273eb87157299a170ab4eca05895700f3f6d
Mark Triggs <mark@dishevelled.net>
2021-03-01 10:38:32 +1100

    Rework hot reloaded properties
    Recently on prod9 we saw a lot of threads stuck in this state:
    
         "http-nio-8080-exec-936::1614268836527" #6255910 daemon prio=5 os_prio=0 tid=0x00007f66e4a68000 nid=0x870c runnable [0x00007f6648290000]
            java.lang.Thread.State: RUNNABLE
                 at java.lang.String.equals(String.java:987)
                 at java.util.Hashtable.get(Hashtable.java:367)
                 - locked <0x000000069168f5d8> (a java.util.Properties)
                 at org.sakaiproject.component.cover.HotReloadConfigurationService.getString(HotReloadConfigurationService.java:82)
                 at org.sakaiproject.component.impl.NYUConfigurationService.getHotReloadProperties(NYUConfigurationService.java:28)
                 - locked <0x00000006915eee68> (a org.sakaiproject.component.impl.NYUConfigurationService)
                 at org.sakaiproject.component.impl.NYUConfigurationService.getString(NYUConfigurationService.java:20)
                 at org.sakaiproject.component.impl.BasicConfigurationService.getString(BasicConfigurationService.java:458)
                 at org.sakaiproject.component.cover.ServerConfigurationService.getString(ServerConfigurationService.java:200)
    
    (actually, one of them was in this state: the rest of them were stuck
    on the first lock)
    
    Not clear whether lock contention was the root cause here, but we can
    use atomic variables to do this without locking anyway.  So we do!

--- kernel/component-manager/src/main/java/org/sakaiproject/component/cover/HotReloadConfigurationService.java
+++ kernel/component-manager/src/main/java/org/sakaiproject/component/cover/HotReloadConfigurationService.java
@@ -1,9 +1,9 @@
 package org.sakaiproject.component.cover;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.FileInputStream;
-import java.util.Properties;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -17,29 +17,45 @@ public class HotReloadConfigurationService
 {
     private static Log LOG = LogFactory.getLog(HotReloadConfigurationService.class);
 
+    // Recheck sakai.properties this often
     private static long MAX_STALE_MS = 60000;
-    private static long lastCheckTime = 0;
-    private static long mtimeSeenAtLastCheck = 0;
 
-    private static Properties _properties;
+    // The time we last checked sakai.properties
+    private static AtomicLong lastCheckTime = new AtomicLong(0);
+
+    // The time we last loaded sakai.properties
+    private static AtomicLong lastRefreshTime = new AtomicLong(0);
+
+    // The modification time on sakai.properties at the time we last loaded it.
+    private static AtomicLong lastSeenFileMtime = new AtomicLong(0);
+
+    // True if we're refreshing right now.
+    private static AtomicBoolean refreshInProgress = new AtomicBoolean(false);
+
+    // The actual properties.  Trimmed of whitespace and ready to go.
+    private static AtomicReference<HashMap<Object, Object>> _properties = new AtomicReference(false);
+
+    // Special properties that should be hot reloaded no matter what.  Refreshed
+    // whenever sakai.properties is reloaded.
+    private static AtomicReference<List<String>> hotReloadOverrideProperties = new AtomicReference(false);
+
+    static {
+        refreshProperties();
+    }
 
 
-    private static synchronized Properties refreshProperties() {
+    public static List<String> getHotReloadOverrideProperties() {
+        maybeRefreshProperties();
+
+        return hotReloadOverrideProperties.get();
+    }
+
+    // Unconditional refresh
+    private static void refreshProperties() {
         long now = System.currentTimeMillis();
-
-        if ((now - lastCheckTime) < MAX_STALE_MS) {
-            // No refresh needed
-            return _properties;
-        }
 
         File sakaiProperties = new File(ServerConfigurationService.getSakaiHomePath() + "/sakai.properties");
         long currentLastModifiedTime = sakaiProperties.lastModified();
-
-        if (mtimeSeenAtLastCheck == currentLastModifiedTime) {
-            // No refresh needed
-            lastCheckTime = now;
-            return _properties;
-        }
 
         LOG.info("Reloading properties file: " + sakaiProperties);
 
@@ -50,9 +66,23 @@ public class HotReloadConfigurationService
             Properties newProperties = new Properties();
             newProperties.load(input);
 
-            _properties = newProperties;
-            lastCheckTime = now;
-            mtimeSeenAtLastCheck = currentLastModifiedTime;
+            HashMap<Object, Object> propertiesMap = new HashMap<>();
+            for (Map.Entry entry : newProperties.entrySet()) {
+                propertiesMap.put(((String)entry.getKey()).trim(), ((String)entry.getValue()).trim());
+            }
+
+            _properties.set(propertiesMap);
+
+            String hotReloadOverrideStr = (String)_properties.get().getOrDefault("nyu.hot-reloadable-properties", null);
+
+            if (hotReloadOverrideStr == null || "".equals(hotReloadOverrideStr)) {
+                hotReloadOverrideProperties.set(Collections.<String>emptyList());
+            } else {
+                hotReloadOverrideProperties.set(Arrays.asList(hotReloadOverrideStr.split(" *, *")));
+            }
+
+            lastRefreshTime.set(now);
+            lastSeenFileMtime.set(currentLastModifiedTime);
         } catch (IOException e) {
             LOG.error("Exception during properties load: " + e);
             e.printStackTrace();
@@ -66,20 +96,40 @@ public class HotReloadConfigurationService
                 }
             }
         }
+    }
 
-        return _properties;
+    private static void maybeRefreshProperties() {
+        long now = System.currentTimeMillis();
+
+        if ((now - lastCheckTime.get()) < MAX_STALE_MS) {
+            // No refresh needed
+            return;
+        }
+
+        if (refreshInProgress.compareAndSet(false, true)) {
+            try {
+                File sakaiProperties = new File(ServerConfigurationService.getSakaiHomePath() + "/sakai.properties");
+                long currentLastModifiedTime = sakaiProperties.lastModified();
+
+                if (lastSeenFileMtime.get() == currentLastModifiedTime) {
+                    // No refresh needed -- file unchanged
+                    lastCheckTime.set(now);
+                    return;
+                }
+
+                // File was changed and we need to refresh
+                refreshProperties();
+            } finally {
+                refreshInProgress.set(false);
+            }
+        }
     }
 
 
     public static String getString(String value, String defaultValue) {
-        Properties properties = refreshProperties();
+        maybeRefreshProperties();
 
-        if (properties == null) {
-            LOG.error("No properties were available.  Returning default value");
-            return defaultValue;
-        }
-
-        String result = (String)properties.get(value);
+        String result = (String)_properties.get().get(value);
 
         if (result == null) {
             return defaultValue;
